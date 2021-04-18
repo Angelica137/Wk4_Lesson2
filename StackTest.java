e7e06c7e2dbb65dd8c906cd574bc0ff89a7753bb
Angelica <Angelica137@users.noreply.github.com>
2021-04-18 23:49:27 +0100

    add the existing array to teh stack

--- StackTest.java
+++ StackTest.java
@@ -9,10 +9,20 @@ import java.util.*;
 public class StackTest {
 	public static void main(String[] args) {
 
-		Stack s = new Stack();
+		Stack<Integer> s = new Stack();
+		for (int i = 1; i <= 6; i++) {
+			s.push(i);
+		}
+		System.out.println(s);
 		s.push(4);
 		s.push(1);
 		s.push(3);
+		System.out.println(s);
+		s.pop();
+		System.out.println(s);
+		s.push(8);
+		System.out.println(s);
+		s.pop();
 		System.out.println(s);
 	}
 }
